4b29d43067f9730056b3af9cc3e8474d58e24326
Elias Taveras <eliastave12@gmail.com>
2020-03-24 23:03:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,9 +10,10 @@ end
 
 def find_max_value(array)
   count = 0
-  
+  number = array[0]
   while count < array.length do
-    if array[count] >
+    if number > array[count]
+      
 end
 
 def find_min_value(array)
